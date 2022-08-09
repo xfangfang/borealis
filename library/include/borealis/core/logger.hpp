93ed9bb645de10a8833a921480b571e540673976
xfangfang <2553041586@qq.com>
2022-08-10 00:27:55 +0800

    Support milliseconds

--- library/include/borealis/core/logger.hpp
+++ library/include/borealis/core/logger.hpp
@@ -44,9 +44,17 @@ class Logger
         if (Logger::logLevel < logLevel)
             return;
 
+        auto now = std::chrono::system_clock::now();
+        uint64_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()
+                                   - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
+        time_t tt = std::chrono::system_clock::to_time_t(now);
+        auto time_tm = localtime(&tt);
+        char strTime[13] = { 0 };
+        sprintf(strTime, "%02d:%02d:%02d.%03d", time_tm->tm_hour, time_tm->tm_min, time_tm->tm_sec, (int)ms);
+
         try
         {
-            fmt::print("\033{}[{}]\033[0m ", color, prefix);
+            fmt::print("{}\033{}[{}]\033[0m ", strTime, color, prefix);
             fmt::print(format, args...);
             fmt::print("\n");
 
