44e3a2a9f5c24734a11313428454309560be10c2
xfangfang <2553041586@qq.com>
2022-09-02 21:10:28 +0800

    Label: Fix line height error

--- library/lib/views/label.cpp
+++ library/lib/views/label.cpp
@@ -433,6 +433,11 @@ void Label::draw(NVGcontext* vg, float x, float y, float width, float height, St
     nvgFontSize(vg, this->fontSize);
     nvgTextAlign(vg, horizAlign | vertAlign);
     nvgFontFaceId(vg, this->font);
+#ifdef __SWITCH__
+    nvgTextLineHeight(vg, this->lineHeight);
+#else
+    nvgTextLineHeight(vg, this->lineHeight * 2 / 3);
+#endif
     nvgTextLineHeight(vg, this->lineHeight);
     nvgFillColor(vg, a(this->textColor));
 
@@ -594,11 +599,11 @@ float Label::getFontSize()
 
 float Label::getLineHeight()
 {
-#ifdef __SWITCH__
+//#ifdef __SWITCH__
     return this->lineHeight;
-#else
-    return this->lineHeight * 2 / 3;
-#endif
+//#else
+//    return this->lineHeight * 2 / 3;
+//#endif
 }
 
 NVGcolor Label::getTextColor()
