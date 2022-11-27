8cf7da999cdf1a032d7e4ebe7a42b3cffb749ef5
xfangfang <2553041586@qq.com>
2022-11-27 15:29:35 +0800

    Remove useless code

--- demo/main.cpp
+++ demo/main.cpp
@@ -22,17 +22,10 @@
 
 #ifdef __PSV__
 #include <borealis/platforms/psv/vita_debug.h>
-extern "C"
-{
-#include <psp2/kernel/sysmem.h>
-#include <EGL/egl1_4.h>
-#include <gpu_es4/psp2_pvr_hint.h>
-#include <psp2/kernel/modulemgr.h>
-}
 #endif
 
-#include <cstdlib>
 #include <borealis.hpp>
+#include <cstdlib>
 #include <string>
 
 #include "captioned_image.hpp"
@@ -56,71 +49,49 @@ int main(int argc, char* argv[])
     brls::Logger::setLogLevel(brls::LogLevel::LOG_DEBUG);
 
 #ifdef __PSV__
-    sceKernelLoadStartModule("vs0:sys/external/libfios2.suprx", 0, nullptr, 0, nullptr, nullptr);
-    sceKernelLoadStartModule("vs0:sys/external/libc.suprx", 0, nullptr, 0, nullptr, nullptr);
-    sceKernelLoadStartModule("app0:module/libgpu_es4_ext.suprx", 0, nullptr, 0, nullptr, nullptr);
-    sceKernelLoadStartModule("app0:module/libIMGEGL.suprx", 0, nullptr, 0, nullptr, nullptr);
-
-    startDebug();
-    brls::Logger::getLogEvent()->subscribe([](const std::string& log) {
-            debug("%s\n", log.c_str());
-    });
-
-
-    PVRSRV_PSP2_APPHINT hint;
-    PVRSRVInitializeAppHint(&hint);
-    PVRSRVCreateVirtualAppHint(&hint);
-
-
-    EGLenum a = eglQueryAPI();
-    if (a == EGL_OPENGL_ES_API){
-        brls::Logger::debug("eglQueryAPI: 1");
-    } else if (a == EGL_OPENGL_API){
-        brls::Logger::debug("eglQueryAPI: 2");
-    } else if (a == EGL_NONE) {
-        brls::Logger::debug("eglQueryAPI: 3");
-    } else {
-        brls::Logger::debug("eglQueryAPI: 4");
-    }
+//    startDebug();
+//    brls::Logger::getLogEvent()->subscribe([](const std::string& log) {
+//            debug("%s\n", log.c_str());
+//    });
 #endif
 
-//    // Init the app and i18n
-//    if (!brls::Application::init())
-//    {
-//        brls::Logger::error("Unable to init Borealis application");
-//        return EXIT_FAILURE;
-//    }
-//
-//    brls::Application::createWindow("demo/title"_i18n);
-//
-//    // Have the application register an action on every activity that will quit when you press BUTTON_START
-//    brls::Application::setGlobalQuit(false);
-//
-//    // Register custom views (including tabs, which are views)
-//    brls::Application::registerXMLView("CaptionedImage", CaptionedImage::create);
-//    brls::Application::registerXMLView("RecyclingListTab", RecyclingListTab::create);
-//    brls::Application::registerXMLView("ComponentsTab", ComponentsTab::create);
-//    brls::Application::registerXMLView("PokemonView", PokemonView::create);
-//    brls::Application::registerXMLView("SettingsTab", SettingsTab::create);
-//
-//    // Add custom values to the theme
-//    brls::Theme::getLightTheme().addColor("captioned_image/caption", nvgRGB(2, 176, 183));
-//    brls::Theme::getDarkTheme().addColor("captioned_image/caption", nvgRGB(51, 186, 227));
-//
-//    // Add custom values to the style
-//    brls::getStyle().addMetric("about/padding_top_bottom", 50);
-//    brls::getStyle().addMetric("about/padding_sides", 75);
-//    brls::getStyle().addMetric("about/description_margin", 50);
-//
-//    // Create and push the main activity to the stack
-//    brls::Application::pushActivity(new MainActivity());
-//
-//    // Run the app
-//    while (brls::Application::mainLoop())
-//        ;
+    // Init the app and i18n
+    if (!brls::Application::init())
+    {
+        brls::Logger::error("Unable to init Borealis application");
+        return EXIT_FAILURE;
+    }
+
+    brls::Application::createWindow("demo/title"_i18n);
+
+    // Have the application register an action on every activity that will quit when you press BUTTON_START
+    brls::Application::setGlobalQuit(false);
+
+    // Register custom views (including tabs, which are views)
+    brls::Application::registerXMLView("CaptionedImage", CaptionedImage::create);
+    brls::Application::registerXMLView("RecyclingListTab", RecyclingListTab::create);
+    brls::Application::registerXMLView("ComponentsTab", ComponentsTab::create);
+    brls::Application::registerXMLView("PokemonView", PokemonView::create);
+    brls::Application::registerXMLView("SettingsTab", SettingsTab::create);
+
+    // Add custom values to the theme
+    brls::Theme::getLightTheme().addColor("captioned_image/caption", nvgRGB(2, 176, 183));
+    brls::Theme::getDarkTheme().addColor("captioned_image/caption", nvgRGB(51, 186, 227));
+
+    // Add custom values to the style
+    brls::getStyle().addMetric("about/padding_top_bottom", 50);
+    brls::getStyle().addMetric("about/padding_sides", 75);
+    brls::getStyle().addMetric("about/description_margin", 50);
+
+    // Create and push the main activity to the stack
+    brls::Application::pushActivity(new MainActivity());
+
+    // Run the app
+    while (brls::Application::mainLoop())
+        ;
 
 #ifdef __PSV__
-    cleanupDebug();
+//    cleanupDebug();
 #endif
     // Exit
     return EXIT_SUCCESS;
