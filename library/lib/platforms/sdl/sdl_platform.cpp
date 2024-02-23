533f88ca47f86856a99c33a36275f440fb3d8670
Daniil Vinogradov <xitrix@bk.ru>
2024-02-23 13:44:42 +0100

    Fix for code compilation

--- library/lib/platforms/sdl/sdl_platform.cpp
+++ library/lib/platforms/sdl/sdl_platform.cpp
@@ -171,6 +171,7 @@ bool SDLPlatform::processEvent(SDL_Event* event)
         auto* manager = this->inputManager;
         if (manager)
             manager->updateKeyboardState(event->key);
+    }
     else if (event->type == SDL_MOUSEMOTION)
     {
         auto* manager = this->inputManager;
