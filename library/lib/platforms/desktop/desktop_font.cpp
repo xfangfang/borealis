1eb4e3e4a3e9f2760751c9658e2f35b337bb9e96
xfangfang <2553041586@qq.com>
2024-05-01 08:29:52 +0800

    DesktopFont: Code refactoring

--- library/lib/platforms/desktop/desktop_font.cpp
+++ library/lib/platforms/desktop/desktop_font.cpp
@@ -21,11 +21,8 @@
 #include <borealis/core/assets.hpp>
 #include <borealis/platforms/desktop/desktop_font.hpp>
 
-#define INTER_FONT "font/switch_font.ttf"
-#define INTER_FONT_PATH BRLS_ASSET(INTER_FONT)
-
-#define INTER_ICON "font/switch_icons.ttf"
-#define INTER_ICON_PATH BRLS_ASSET(INTER_ICON)
+#define INTER_FONT_PATH BRLS_ASSET("font/switch_font.ttf")
+#define INTER_ICON_PATH BRLS_ASSET("font/switch_icons.ttf")
 
 namespace brls
 {
@@ -53,37 +50,49 @@ bool DesktopFontLoader::loadFontsExist(NVGcontext* vg, std::vector<std::string> 
     return false;
 }
 
+bool DesktopFontLoader::loadFont(const std::string& name, const std::string& path) {
+#ifdef USE_LIBROMFS
+    if (path.empty()) return false;
+    if (path.rfind("@res/", 0) == 0)
+    {
+        // font is inside the romfs
+        try
+        {
+            auto& font = romfs::get(path.substr(5));
+            if (font.valid() && Application::loadFontFromMemory(name, (void*)font.data(), font.size(), false))
+                return true;
+        }
+        catch (...)
+        {
+        }
+    } else
+#endif
+    if (access(path.c_str(), F_OK) != -1 && Application::loadFontFromFile(name, path)) {
+        return true;
+    }
+
+    return false;
+}
+
 void DesktopFontLoader::loadFonts()
 {
     NVGcontext* vg = brls::Application::getNVGContext();
 
-    // Regular
-    // Try to use user-provided font first, fallback to Inter
-    if (access(USER_FONT_PATH.c_str(), F_OK) != -1 && this->loadFontFromFile(FONT_REGULAR, USER_FONT_PATH))
+    // Text font
+    if (loadFont(FONT_REGULAR, USER_FONT_PATH))
     {
-        brls::Logger::info("Load custom font: {}", USER_FONT_PATH);
-
-        // Add internal font as fallback
-#ifdef USE_LIBROMFS
-        auto& font = romfs::get(INTER_FONT);
-        if (font.valid() && Application::loadFontFromMemory("default", (void*)font.data(), font.size(), false))
-#else
-        if (this->loadFontFromFile("default", INTER_FONT_PATH))
-#endif
+        // Using internal font as fallback
+        if (loadFont("default", INTER_FONT_PATH))
         {
             nvgAddFallbackFontId(vg, Application::getFont(FONT_REGULAR), Application::getFont("default"));
         }
-    }
-    else
-    {
+    } else {
         brls::Logger::warning("Cannot find custom font, (Searched at: {})", USER_FONT_PATH);
-        brls::Logger::info("Using internal font: {}", INTER_FONT_PATH);
-#ifdef USE_LIBROMFS
-        auto& font = romfs::get(INTER_FONT);
-        Application::loadFontFromMemory(FONT_REGULAR, (void*)font.data(), font.size(), false);
-#else
-        this->loadFontFromFile(FONT_REGULAR, INTER_FONT_PATH);
-#endif
+        brls::Logger::info("Trying to use internal font: {}", INTER_FONT_PATH);
+        if (!loadFont(FONT_REGULAR, INTER_FONT_PATH))
+        {
+            Logger::warning("Failed to load internal font, text may not be displayed");
+        }
     }
 
     // Using system font as fallback
@@ -124,67 +133,29 @@ void DesktopFontLoader::loadFonts()
     }
 
     // Load Emoji
-    if (!USER_EMOJI_PATH.empty())
+    if (loadFont(FONT_EMOJI, USER_EMOJI_PATH))
     {
-        if (access(USER_EMOJI_PATH.c_str(), F_OK) != -1)
-        {
-            brls::Logger::info("Load emoji font: {}", USER_EMOJI_PATH);
-            this->loadFontFromFile("emoji", USER_EMOJI_PATH);
-            nvgAddFallbackFontId(vg, Application::getFont(FONT_REGULAR), Application::getFont("emoji"));
-        }
+        nvgAddFallbackFontId(vg, Application::getFont(FONT_REGULAR), Application::getFont(FONT_EMOJI));
     }
 
-    // Switch icons
-    // Only supports user-provided font
-#ifdef USE_LIBROMFS
-    bool loaded = false;
-    if (USER_ICON_PATH.rfind("@res/", 0) == 0)
+    // bottom bar icons
+    if (loadFont(FONT_SWITCH_ICONS, USER_ICON_PATH))
     {
-        // USER_ICON_PATH is inside the romfs
-        try
-        {
-            auto& icon = romfs::get(USER_ICON_PATH.substr(5));
-            if (icon.valid())
-            {
-                loaded = Application::loadFontFromMemory(FONT_SWITCH_ICONS, (void*)icon.data(), icon.size(), false);
-            }
-        }
-        catch (...)
-        {
-        }
-    } else if (access(USER_ICON_PATH.c_str(), F_OK) != -1) {
-        // USER_ICON_PATH is an external path
-        loaded = this->loadFontFromFile(FONT_SWITCH_ICONS, USER_ICON_PATH);
-    }
-
-    if (loaded)
-#else
-    if (access(USER_ICON_PATH.c_str(), F_OK) != -1 && this->loadFontFromFile(FONT_SWITCH_ICONS, USER_ICON_PATH))
-#endif
-    {
-        brls::Logger::info("Load keymap icon: {}", USER_ICON_PATH);
+        // User-provided icons
         nvgAddFallbackFontId(vg, Application::getFont(FONT_REGULAR), Application::getFont(FONT_SWITCH_ICONS));
     }
     else
     {
         brls::Logger::warning("Cannot find custom icon, (Searched at: {})", USER_ICON_PATH);
-#ifdef USE_LIBROMFS
-        // If you do not want to put a default icons in your own application,
-        // you can leave an empty icon file in the resource folder to avoid errors reported by libromfs.
-        auto& icon = romfs::get(INTER_ICON);
-        // Determine if the file is empty
-        if (icon.valid() && Application::loadFontFromMemory(FONT_SWITCH_ICONS, (void*)icon.data(), icon.size(), false))
+        brls::Logger::info("Trying to use internal icon: {}", INTER_ICON_PATH);
+        if (loadFont(FONT_SWITCH_ICONS, INTER_ICON_PATH))
         {
-#else
-        if (access(INTER_ICON_PATH, F_OK) != -1 && this->loadFontFromFile(FONT_SWITCH_ICONS, INTER_ICON_PATH))
-        {
-#endif
+            // Internal icons
             nvgAddFallbackFontId(vg, Application::getFont(FONT_REGULAR), Application::getFont(FONT_SWITCH_ICONS));
-            brls::Logger::info("Using internal icon: {}", INTER_ICON_PATH);
         }
         else
         {
-            Logger::warning("Icons may not be displayed, for more information please refer to: https://github.com/xfangfang/wiliwili/discussions/38");
+            Logger::warning("Failed to load internal icons, bottom bar icons may not be displayed");
         }
     }
 
