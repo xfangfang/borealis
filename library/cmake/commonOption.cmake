4046de507c725f0967b1a232a284837624deef44
xfangfang <2553041586@qq.com>
2025-03-31 22:22:04 +0800

    Enable fontstash stream by default

--- library/cmake/commonOption.cmake
+++ library/cmake/commonOption.cmake
@@ -64,7 +64,7 @@ option(SIMPLE_HIGHLIGHT "Simple highlight" OFF)
 # https://cmake.org/cmake/help/latest/prop_tgt/UNITY_BUILD.html
 option(BRLS_UNITY_BUILD "Unity build" OFF)
 
-option(BRLS_FONTSTASH_STREAM "fontstash use stream" OFF)
+option(BRLS_FONTSTASH_STREAM "fontstash use stream" ON)
 
 if (NOT DEFINED APP_PLATFORM_INCLUDE)
     set(APP_PLATFORM_INCLUDE)
